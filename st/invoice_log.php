d8881b86c40b62cfe1baa4607e1c62964e6a21c3
HARI MOHAN THAKUR <emailharimohan@gmail.com>
2024-10-05 22:51:59 +0530

    Update invoice_log.php

--- st/invoice_log.php
+++ st/invoice_log.php
@@ -1,5 +1,5 @@
 <?php
-require '../env.php';
+// require '../env.php';
 
 /*
 Read POST data
@@ -7,18 +7,18 @@ reading posted data directly from $_POST causes serialization
 issues with array data in POST.
 Reading raw POST data from input stream instead.
 */
-define("IPN_LOG_FILE", "ipn.log");
-$raw_post_data = file_get_contents('php://input');
+// define("IPN_LOG_FILE", "ipn.log");
+// $raw_post_data = file_get_contents('php://input');
 
-error_log(date('[Y-m-d H:i e] '). 
-		"post params IPN:==========" . PHP_EOL, 3, IPN_LOG_FILE);
+// error_log(date('[Y-m-d H:i e] '). 
+// 		"post params IPN:==========" . PHP_EOL, 3, IPN_LOG_FILE);
 
-error_log(date('[Y-m-d H:i e] '). 
-		"post params IPN: $raw_post_data" . PHP_EOL, 3, IPN_LOG_FILE);
+// error_log(date('[Y-m-d H:i e] '). 
+// 		"post params IPN: $raw_post_data" . PHP_EOL, 3, IPN_LOG_FILE);
 		
 		
-error_log(date('[Y-m-d H:i e] '). 
-		"POST IPN: $_POST" . PHP_EOL, 3, IPN_LOG_FILE);
-		
+// error_log(date('[Y-m-d H:i e] '). 
+// 		"POST IPN: $_POST" . PHP_EOL, 3, IPN_LOG_FILE);
+error_log(date('[Y-m-d H:i e] ')."POST IPN: $_REQUEST" . PHP_EOL, 3, IPN_LOG_FILE);		
 	
 ?>
