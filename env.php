bd92432b3efdc35bcc6628136783f344c8567423
shalumall9 <89930761+shalumall9@users.noreply.github.com>
2024-10-18 19:42:50 +0530

    Update env.php

--- env.php
+++ env.php
@@ -11,6 +11,25 @@ if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
     define("STRIPE_SECRET_API_KEY", "sk_test_51LMwZcSGy15lQVN7edh0JDonnRF9zvonb5fxiLsyxAjrKG1JRYPuDcS7Ip8TjJsgar4oJCU15il3CNTun64gLc2o00GLEeoVoh");
     define("STRIPE_PUBLISHABLE_KEY", "pk_test_51LMwZcSGy15lQVN78JtOVUnYXGNBnP0FmZmuJy3QpjmfFDmDNgBdAqu03ibCmYHwZN5E1pBXnmVH3DnSF3cJRzA700iRRRswpm");
 
+     // PayPal Configuration
+    define('PAYPAL_EMAIL', 'cservendra@gmail.com'); 
+    define('DOMAIN_LINK', 'http://indogen.in');
+    define('RETURN_URL', 'http://indogen.in/pp/return.php'); 
+    define('CANCEL_URL', 'http://indogen.in/pp/cancel.php'); 
+    define('NOTIFY_URL', 'http://indogen.in/pp/notify.php'); 
+    define('CURRENCY', 'USD'); 
+    define('SANDBOX', FALSE); // TRUE or FALSE 
+    define('LOCAL_CERTIFICATE', FALSE); // TRUE or FALSE
+
+    if (SANDBOX === TRUE){
+        $paypal_url = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+    }else{
+        $paypal_url = "https://www.paypal.com/cgi-bin/webscr";
+    }
+    // PayPal IPN Data Validate URL
+    define('PAYPAL_URL', $paypal_url);
+    
+
 } else {
    
     define('DB_HOST', 'localhost'); 
@@ -23,7 +42,29 @@ if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
     define("STRIPE_SECRET_API_KEY", "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");
     define("STRIPE_PUBLISHABLE_KEY", "pk_live_51LMwZcSGy15lQVN71VPcBLI8V7UFVn0rwPaxiKH6dBrZG9BmJt0at9mMBo6ov2QPQoIB1FcKXkWV8xwuu2BFMwMb00Bu2WrBOe");
 
+     // PayPal Configuration
+    define('PAYPAL_EMAIL', 'cservendra@gmail.com'); 
+    define('DOMAIN_LINK', 'https://test.indogenmed.in'); 
+    define('RETURN_URL', 'https://test.indogenmed.in/pp/return.php'); 
+    define('CANCEL_URL', 'https://test.indogenmed.in/pp/cancel.php'); 
+    define('NOTIFY_URL', 'https://test.indogenmed.in/pp/notify.php'); 
+    define('CURRENCY', 'USD'); 
+    define('SANDBOX', FALSE); // TRUE or FALSE 
+    define('LOCAL_CERTIFICATE', FALSE); // TRUE or FALSE
+
+    if (SANDBOX === TRUE){
+        $paypal_url = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+    }else{
+        $paypal_url = "https://www.paypal.com/cgi-bin/webscr";
+    }
+    // PayPal IPN Data Validate URL
+    define('PAYPAL_URL', $paypal_url);
+
 }
 
+
+// WISE TEST TOKEN
+define('WISE_TOKEN', '637591a3-d9bc-47b2-bfde-cc33d20adfd3'); 
+define('YOUR_SOURCE_ACCOUNT_ID', ''); 
 
 ?>
